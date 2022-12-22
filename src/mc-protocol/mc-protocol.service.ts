6705f6bb844499c7b1213de976af7dbb47ae52ba
toanpham <toanphambk@gmail.com>
2022-12-23 01:24:34 +0700

    first commit

--- src/mc-protocol/mc-protocol.service.ts
+++ src/mc-protocol/mc-protocol.service.ts
@@ -375,6 +375,7 @@ export class McProtocolService {
     let str = '';
     for (let i = 0; i < hex.length; i += 2)
       str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    str.replace(/\0/g, '');
     return str;
   }
 }
